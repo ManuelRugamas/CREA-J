f102627b58beb1a5e3fbc15f60e60f8537120985
ManuelRugamas <estudiante20140018@cdb.edu.sv>
2026-03-12 15:50:22 -0600

    Add files via upload

---
+++ CREA J/Inicio.js
@@ -0,0 +1,21 @@
+let cuenta = []
+
+function guardar () {
+
+    let acc_name = document.getElementById("acc_name").value;
+    let acc_pass = document.getElementById("acc_pass").value;
+
+      if (acc_name === "" || acc_pass === "") {
+                alert("Todos los campos son obligatorios");
+            } else {
+                let persona = {
+                    nombre: nombre,
+                    apellido: apellido,
+                    edad: edad,
+                    placa: placa,
+                    marca: marca,
+                    modelo: modelo,
+                };
+
+            }
+    }
